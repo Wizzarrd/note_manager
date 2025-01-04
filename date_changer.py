dd8164f473a71f602007ca49dd44a5deff1f7e28
Wizzarrd <vitalyr2002@gmail.com>
2025-01-04 16:36:31 +0700

    Add files via upload

---
+++ date_changer.py
@@ -0,0 +1,5 @@
+created_date = '28-12-2024'
+issue_date = '31-12-2024'
+temp_issue_date = issue_date[0:-5]
+temp_created_date = created_date[0:-5]
+print(temp_created_date, temp_issue_date, sep=', ')
