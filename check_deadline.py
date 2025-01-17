d5a734882927fb0f0bc618694a757d7bf0231585
Wizzarrd <vitalyr2002@gmail.com>
2025-01-17 19:13:34 +0700

    Add files via upload

--- check_deadline.py
+++ check_deadline.py
@@ -1,26 +1,21 @@
-# Создаём словарь со значениями статуса заметок и объявляем текущий статус.
-status_list = {'1' : 'Выполнено', '2' : 'В процессе', '3' : 'Отложено'}
-current_status = status_list.get('2')
-print('Текущий статус заметки: ', current_status)
-# Даём пользователю выбрать новый статус заметки.
-print('Ввыберите новый статус заметки:\n'
-      '1. Выполнено\n'
-      '2. В процессе\n'
-      '3. Отложено')
-current_status_input = input('Ваш выбор: ')
-# Поиск соответствующих значений статуса по ключам и значениям
-# из словаря и проверка ввода на корректность
+import datetime
+
+# Вывод текущей даты.
+current_date = datetime.date.today()
+print('Текущая дата: ', current_date.strftime("%d-%m-%Y"))
+# Запрос даты дедлайна и её анализ.
 while True:
-    # Проверка на соответствие ключам
-    if status_list.get(current_status_input) is not None:
-        current_status = status_list.get(current_status_input)
-        break
-    # Проверка на соответствие значениям
-    elif current_status_input in status_list.values():
-        current_status = current_status_input
-        break
+    issue_date_str = input('Введите дату дедлайна в формате ДД-ММ-ГГГГ: ')
+    try:
+        issue_date = datetime.datetime.strptime(issue_date_str, '%d-%m-%Y').date()
+    except:
+        print('Вы ввели дату неверно! Используйте предложенный формат. Пример: 10-12-2024')
     else:
-        current_status_input = input('Некорректный ввод. \n'
-                                     'Статус заметки может быть: "Выполнено", "В процессе", "Отложено".\n'
-                                     'Введите один из предложенных вариантов: ')
-print('Статус заметки успешно изменён на: ', current_status)
+        break
+# Сравниваем даты.
+if current_date < issue_date:
+    print(f'До дедлайна осталось {(issue_date - current_date).days} дней.')
+elif current_date == issue_date:
+    print('Дедлайн сегодня! Поторопитесь!')
+else:
+    print(f'Дедлайн истёк {(current_date - issue_date).days} дней назад.')
